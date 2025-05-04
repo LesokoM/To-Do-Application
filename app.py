1eb7295a598f236c29d27d2e2d4cbded86fc0fd6
LesokoM <shokijhb@gmail.com>
2025-05-04 14:19:17 +0200

    db creation
    Very confusing
    Need to explore how sessions and getting the info works

--- app.py
+++ app.py
@@ -7,6 +7,39 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 
 
+def initialise_db():
+    conn = sqlite3.connect('app.db')
+    cursor = conn.cursor()
+
+    cursor.execute(
+        '''
+        CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY,
+        username TEXT UNIQUE,
+        password TEXT
+        )''')
+
+    cursor.execute(
+        '''
+        CREATE TABLE IF NOT EXISTS tasks(
+        id INTEGER PRIMARY KEY,
+        user_id INTEGER,
+        task_name TEXT,
+        due_date TEXT,
+        completed BOOLEAN DEFAULT 0,
+        FOREIGN KEY(user_id) REFERENCES users(id)
+        )''')
+    conn.commit()
+    conn.close()
+
+
+
+
+
+
+
+initialise_db()
+
 def get_db_connection():
     cursor = sqlite3.connect('app.db')
     cursor.row_factory = sqlite3.Row
@@ -39,11 +72,12 @@ def submit_form():
         username = request.form["username"]
         password = request.form["password"]
         cursor = get_db_connection()
-        user = cursor.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username,password)).fetone()
-        cursor.close()
 
-        if user:
-            return redirect(url_for("todolist"))
+        # check if that username and password is actually in the system 
+        # if yes then go to do list and populate it with whatever is linked to that user
+        user = cursor.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username,password)).fetchone()
+        cursor.close()
+        return redirect(url_for("todolist"))
 
 
     return redirect(url_for("login")) #should return to sign up page
